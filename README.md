06b375c946857fb4fd8d97ba907ce3990f11b88b
Miłosz Sieradzki <milosz+github@sieradzki.it>
2014-03-29 14:43:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Youtrack polish keybindings fixer
+=================================
---
+++ youtrackPolishKeybindingsFixer.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Youtrack polish keybindings fixer
+// @namespace   myjetbrains.com
+// @description Naprawia skróty klawiszowe do wstawiania polskich znaków w polach tekstowych Youtrack.
+// @version     1.0
+// @include     http://*.myjetbrains.com/youtrack*
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+})();
