9610dde32638189d8e5f043d394ac9b446271538
Miłosz Sieradzki <milosz+github@sieradzki.it>
2014-05-19 21:18:13 +0200

    Fix requested permissions to web pages

--- youtrackPolishKeybindingsFixer.user.js
+++ youtrackPolishKeybindingsFixer.user.js
@@ -2,8 +2,8 @@
 // @name        Youtrack polish keybindings fixer
 // @namespace   myjetbrains.com
 // @description Naprawia skróty klawiszowe do wstawiania polskich znaków w polach tekstowych Youtrack.
-// @version     1.0
-// @include     http://*.myjetbrains.com/youtrack*
+// @version     1.0.1
+// @match       http://*.myjetbrains.com/youtrack*
 // ==/UserScript==
 
 (function () {
